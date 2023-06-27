8a2057b4e4490a32ca19a39d532182118fc94bd3
Javiera Méndez C <107318002+jamendezc@users.noreply.github.com>
2023-06-26 23:21:45 -0400

    Update wp-settings.php

--- wp-settings.php
+++ wp-settings.php
@@ -1,26 +1,43 @@
-Archivo que se encarga de cargar y configurar todo lo necesario para que WordPress funcione
 <?php
-#Definimos el nombre de la base de datos de WordPress.
+#Define el nombre de la base de datos de WordPress.
 define( 'DB_NAME', 'mybasededatos' );
 
+#Define el nombre de usuario de la base de datos.
 define( 'DB_USER', 'admin' );
+
+#Define la contraseña del usuario de la base de datos.
 define( 'DB_PASSWORD', 'Duoc.2023' );
+
+#Define el host de la base de datos. 
+#En este caso, se utiliza 'punto de conección de la instancia de la base de datos en AWS' 
+#Para indicar que la base de datos se encuentra en el mismo servidor que WordPress.
 define( 'DB_HOST', 'prueba2-instance-1.c0p67o0eorpd.us-east-1.rds.amazonaws.com' );
+
+#Define el juego de caracteres utilizado por la base de datos.
 define( 'DB_CHARSET', 'utf8' );
+
+#Define la configuración de ordenamiento de la base de datos. En este caso, se utiliza el valor por defecto.
 define( 'DB_COLLATE', '' );
 
+#Estas líneas definen las claves únicas utilizadas para mejorar la seguridad de WordPress. 
 define( 'AUTH_KEY', 'Duoc.2023' );
 define( 'SECURE_AUTH_KEY', 'Duoc.2023' );
 define( 'LOGGED_IN_KEY', 'Duoc.2023' );
 define( 'NONCE_KEY', 'Duoc.2023' );
 
+#Define el prefijo de la tabla de la base de datos de WordPress. El prefijo predeterminado es 'wp_'.
 $table_prefix = 'wp_';
 
+#Define el prefijo de la tabla de la base de datos de WordPress. El prefijo predeterminado es 'wp_'.
 define( 'WP_DEBUG', false );
+
+#Define el límite de memoria para WordPress.
 define( 'WP_MEMORY_LIMIT', '64M' );
 
+#Define la ruta absoluta de acceso a los archivos de WordPress.
 if ( ! defined( 'ABSPATH' ) ) {
     define( 'ABSPATH', __DIR__ . '/' );
 }
 
+# Incluye el archivo 'wp-settings.php', que carga la configuración principal de WordPress y realiza las inicializaciones necesarias.
 require_once ABSPATH . 'wp-settings.php';
