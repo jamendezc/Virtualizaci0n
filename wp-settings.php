64881109fe697e89e21dec255a1dd3ca0c1276f3
Javiera Méndez C <107318002+jamendezc@users.noreply.github.com>
2023-06-26 23:04:59 -0400

    Update wp-settings.php
    Archivo que se encarga de cargar y configurar todo lo necesario para que WordPress funcione

--- wp-settings.php
+++ wp-settings.php
@@ -1,23 +1,26 @@
+Archivo que se encarga de cargar y configurar todo lo necesario para que WordPress funcione
 <?php
-define( 'DB_NAME', 'mybasededat<?php;
+#Definimos el nombre de la base de datos de WordPress.
 define( 'DB_NAME', 'mybasededatos' );
-define( 'DB_USER', 'admin' );2023' );
-define( 'DB_PASSWORD', 'Duoc.2023' );1.c0p67o0eorpd.us-east-1.rds.amazonaws.com'                                                                                                             define( 'DB_HOST', 'prueba2-instance-1.c0p67o0eorpd.us-east-1.rds.amazonaws.com'                                                                                                              );ine( 'DB_CHARSET', 'utf8' );
+
+define( 'DB_USER', 'admin' );
+define( 'DB_PASSWORD', 'Duoc.2023' );
+define( 'DB_HOST', 'prueba2-instance-1.c0p67o0eorpd.us-east-1.rds.amazonaws.com' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
+
 define( 'AUTH_KEY', 'Duoc.2023' );
-define( 'AUTH_KEY', 'Duoc.2023' );023' );
 define( 'SECURE_AUTH_KEY', 'Duoc.2023' );
 define( 'LOGGED_IN_KEY', 'Duoc.2023' );
 define( 'NONCE_KEY', 'Duoc.2023' );
+
 $table_prefix = 'wp_';
-$table_prefix = 'wp_';
+
 define( 'WP_DEBUG', false );
-define( 'WP_DEBUG', false );64M' );
 define( 'WP_MEMORY_LIMIT', '64M' );
+
 if ( ! defined( 'ABSPATH' ) ) {
-if ( ! defined( 'ABSPATH' ) ) {. '/' );
     define( 'ABSPATH', __DIR__ . '/' );
 }
-require_once ABSPATH . 'wp-settings.php';
+
 require_once ABSPATH . 'wp-settings.php';
