5157b27a46689596a85aee9de74eb2d2f7156a02
Javiera Méndez C <107318002+jamendezc@users.noreply.github.com>
2023-06-27 22:57:59 -0400

    Update wp-settings.php

--- wp-settings.php
+++ wp-settings.php
@@ -1,14 +1,16 @@
+#todo lo que este con * se debe cambiar
+
 <?php
-#Define el nombre de la base de datos de WordPress.
+#Define el nombre de la base de datos que se creaste por linea de comando.*
 define( 'DB_NAME', 'mybasededatos' );
 
-#Define el nombre de usuario de la base de datos.
+#Define el nombre de usuario de la base de datos.*
 define( 'DB_USER', 'admin' );
 
-#Define la contraseña del usuario de la base de datos.
+#Define la contraseña del usuario de la base de datos.*
 define( 'DB_PASSWORD', 'Duoc.2023' );
 
-#Define el host de la base de datos. 
+#Define el host de la base de datos.*
 #En este caso, se utiliza 'punto de conexion instancia RDS' 
 #Para indicar que la base de datos se encuentra en el mismo servidor que WordPress.
 define( 'DB_HOST', 'prueba2-instance-1.c0p67o0eorpd.us-east-1.rds.amazonaws.com' );
@@ -19,7 +21,7 @@ define( 'DB_CHARSET', 'utf8' );
 #Define la configuración de ordenamiento de la base de datos. En este caso, se utiliza el valor por defecto.
 define( 'DB_COLLATE', '' );
 
-#Estas líneas definen las claves únicas utilizadas para mejorar la seguridad de WordPress. 
+#Estas líneas definen las claves únicas utilizadas para mejorar la seguridad de WordPress. *
 define( 'AUTH_KEY', 'Duoc.2023' );
 define( 'SECURE_AUTH_KEY', 'Duoc.2023' );
 define( 'LOGGED_IN_KEY', 'Duoc.2023' );
